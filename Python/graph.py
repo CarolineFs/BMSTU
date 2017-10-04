4917603cb6438e881f14be48d38f34cfc33d9cc1
The Light Betray Me <dr_gromova1998@mail.ru>
2017-10-04 18:27:35 +0300

    Update graph.py

--- Python/graph.py
+++ Python/graph.py
@@ -53,10 +53,9 @@ print('\nКоличество отрицательных значений фун
     
 f = 0
 x = round(first, 7)
-m = 27
-if max1 < (arg//2+1)+m:
-    m = max1
-
+max1 = round(max1)
+max2= round(max2)
+    
 if k2 > 0 or (k1 == 0 and k2 == 0):
     print('\nГрафик функции q2 = x^3 - x - 1: \n')
     m = 27
@@ -80,17 +79,17 @@ if k2 > 0 or (k1 == 0 and k2 == 0):
                 f = 1
                 if q2 > 0:
                     q2 *= -1
-                    print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+(q2-1)+'\u2500'+'*'+((arg//2+1)+25-q2)*'\u2500'+' y')
+                    print('x      ', (len(s)-2)*'\u2500'+'\u253C'+(q2-1)+'\u2500'+'*'+(len(s)-q2-2)*'\u2500'+' y')
                 elif q2 == 0:
-                    print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+'*'+((arg//2+1)+20)*'\u2500'+' y')
+                    print('x      ', (len(s)-2)*'\u2500'+'\u253C'+'*'+(len(s)-2)*'\u2500'+' y')
                 elif q2 < 0:
                     q2 *= -1
-                    print('x      ', ((arg//2+1)+25-q2-1)*'\u2500'+'*'+q2*'\u2500'+'\u253C'+((arg//2+1)+25)*'\u2500'+' y')
+                    print('x      ', (len(s)-q2-3)*'\u2500'+'*'+q2*'\u2500'+'\u253C'+(len(s)-2)*'\u2500'+' y')
             else:
                 if f ==0:
-                    print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+((arg//2+1)+25)*'\u2500'+' y')
+                    print('x      ', (len(s)-2)*'\u2500'+'\u253C'+(len(s)-2)*'\u2500'+' y')
                     for j in range(round(first)-1):
-                        print(34*' '+'\u2502')
+                        print((6+len(s))*' '+'\u2502')
                     f = 1
                 if q2 > 0:
                     q2 *= -1
@@ -99,10 +98,15 @@ if k2 > 0 or (k1 == 0 and k2 == 0):
                     print('{:0.3f} '.format(x)+len(s)*' '+'*')
                 elif q2 < 0:
                     q2 *= -1
-                    print('{:0.3f}  '.format(x)++(len(s)-q2-1)*' '+'*'+(q2-1)*' '+'\u2502')
+                    print('{:0.3f}  '.format(x)+(len(s)-q2-1)*' '+'*'+(q2-1)*' '+'\u2502')
         x = round(x + step, 7)
 elif k1 > 0 and k2 < 0:
     print('\nГрафик функции q1 = x^x + 2x - 6: \n')
+    m = 27
+    s = ((arg//2+1)+m)*'\u0000'
+    if max1 > (arg//2+1)+m:
+        m = max1
+        s = m*'\u0000'
     for i in range(arg):
         q1 = round(copysign(abs(x)**abs(x), x) + 2*x - 6)
         if x < 0:
@@ -119,17 +123,17 @@ elif k1 > 0 and k2 < 0:
                 f = 1
                 if q1 > 0:
                     q1 *= -1
-                    print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+(q1-1)+'\u2500'+'*'+((arg//2+1)+25-q1)*'\u2500'+' y')
+                    print('x      ', (len(s)-2)*'\u2500'+'\u253C'+(q1-1)+'\u2500'+'*'+(len(s)-2-q1)*'\u2500'+' y')
                 elif q1 == 0:
-                    print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+'*'+((arg//2+1)+20)*'\u2500'+' y')
+                    print('x      ', (len(s)-2)*'\u2500'+'\u253C'+'*'+(len(s)-2)*'\u2500'+' y')
                 elif q1 < 0:
                     q1 *= -1
-                    print('x      ', ((arg//2+1)+25-q1-1)*'\u2500'+'*'+q1*'\u2500'+'\u253C'+((arg//2+1)+25)*'\u2500'+' y')
+                    print('x      ', (len(s)-q1-3)*'\u2500'+'*'+q1*'\u2500'+'\u253C'+(len(s)-2)*'\u2500'+' y')
             else:
                 if f ==0:
-                    print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+((arg//2+1)+25)*'\u2500'+' y')
+                    print('x      ', (len(s)-2)*'\u2500'+'\u253C'+(len(s)-2)*'\u2500'+' y')
                     for j in range(round(first)):
-                        print(34*' '+'\u2502')
+                        print((6+len(s))*' '+'\u2502')
                     f = 1
                 if q1 > 0:
                     q1 *= -1
@@ -138,11 +142,11 @@ elif k1 > 0 and k2 < 0:
                     print('{:0.3f} '.format(x)+len(s)*' '+'*')
                 elif q1 < 0:
                     q1 *= -1
-                    print('{:0.3f}  '.format(x)++(len(s)-q1-1)*' '+'*'+(q1-1)*' '+'\u2502')
+                    print('{:0.3f}  '.format(x)+(len(s)-q1-1)*' '+'*'+(q1-1)*' '+'\u2502')
         x = round(x + step, 7)
 if f == 0:
         for j in range(round(last)*-1):
-            print(34*' '+'\u2502')
-        print('x      ', ((arg//2+1)+25)*'\u2500'+'\u253C'+((arg//2+1)+25)*'\u2500'+' y')
+            print((len(s)+6)*' '+'\u2502')
+        print('x      ', (len(s)-2)*'\u2500'+'\u253C'+(len(s)-2)*'\u2500'+' y')
         f = 1
 print((6+len(s))*'\u0000'+'x')
