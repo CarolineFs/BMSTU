f73821651513b5d2d1ce5d19c213bea9b91829f5
The Light Betray Me <dr_gromova1998@mail.ru>
2017-11-17 17:07:10 +0300

    Update and rename Integral_not_working.py to Integral_checked.py

--- Python/Integral_checked.py
+++ Python/Integral_checked.py
@@ -19,7 +19,7 @@ i - служебная
 from math import log
 
 def f(x):
-    return 1/log(x)
+    return log(x)
 
 def leftRectangle(f, a, b, N):
     s = 0
@@ -37,7 +37,7 @@ def trapeze(f, a, b, N):
     s *= h
     return s
 
-def main():
+def printer(a, b, N1, N2):
     print()
     print(18*' '+'\u2502'+'{:^18.4f}'.format(N1)+\
                  '\u2502'+'{:^18.4f}'.format(N2))
@@ -50,14 +50,14 @@ def main():
     print('{:^18}'.format('Метод')+'\u2502'+\
           18*' '+'\u2502')
     print('{:^18}'.format('правых')+'\u2502'+\
-          '{:^18.4f}'.format(I1)+'\u2502'
-          '{:^18.4f}'.format(I1))
+          '{:^18.5f}'.format(I1)+'\u2502'
+          '{:^18.5f}'.format(I2))
     print('{:^18}'.format('прямоугольников')+'\u2502'+\
           18*' '+'\u2502')
     print(line)
     print('{:^18}'.format('Метод')+'\u2502'+\
-          '{:^18.4f}'.format(I3)+'\u2502'
-          '{:^18.4f}'.format(I4))
+          '{:^18.5f}'.format(I3)+'\u2502'
+          '{:^18.5f}'.format(I4))
     print('{:^18}'.format('трапеций')+'\u2502'+\
           18*' '+'\u2502')
     print()
@@ -65,15 +65,15 @@ def main():
 
 #уточние для метода левых прямоугольников
 def refinement(eps):
-    n = 2
+    n = 1
     while True:
         I1 = leftRectangle(f, a, b, n)
         I2 = leftRectangle(f, a, b, n*2)
-        if I2 - I1 < eps:
+        if abs(I2 - I1) < eps:
             for i in range(n, n*2):
                 I3 = leftRectangle(f, a, b, n)
                 I4 = leftRectangle(f, a, b, n+1)
-                if I4 - I3 <eps:
+                if I4 - I3 < eps:
                     res = n + 1
                     break
             print('Чтобы достичь точности '+str(eps)+\
@@ -86,31 +86,62 @@ def CatchIntError(c):
         c = int(c)
     else:
         print('Некорректный ввод')
-        
-def getInterval():
-    a = input('Введите начальное значение: ')
-    b = input('Введите конечное значение: ')
+    return c
 
-def getStep():
-    print('Чтобы выйти, нажмите ENTER.')
-    N1 = input('Введите N1: ')
-    if N1 = '':
-        return
+def CatchFloatError(c):
+    if c.isdigit():
+        return float(c)
     else:
-        CatchIntError(N1)
+        n = ''
+        nums = '0123456789-'
+        for i in range(len(c)):
+            if i != 0 and i != len(c) - 1 and c[i] in '0123456789':
+                n += c[i]
+            elif i == 0 and c[i] in nums:
+                n += c[i]
+            elif i == len(c)-1 and i != 0 and (c[i] in nums[0:10] or c[i] == '.'):
+                n += c[i]
+            elif i != 0 and i != len(c)-1 and c[i] == '.':
+                if '.' not in n and 'e' not in n:
+                    n += c[i]
+            elif i != 0 and i != len(c)-1 and c[i] == '-':
+                if c[i-1] == 'e':
+                    n += c[i]
+            elif i != 0 and i != len(c)-1 and c[i] == 'e':
+                if 'e' not in n:
+                    n += c[i]
+            else:   
+                break
+        if len(n) == len(c) and len(n) != 0:
+            n = float(n)
+            return n
+        else:
+            print('Некорректный ввод. ')
+
+N1 = input('Введите N1: ')
+N1 = CatchIntError(N1)
+if type(N1) is int:
     N2 = input('ведите N2: ')
-    CatchIntError(N2)
-    if type(N1) is str or type(N2) is str:
-        getStep()
-        
+    N2 = CatchIntError(N2)
+    if type(N2) is int:
+        a = input('Введите начальное значение: ')
+        a = CatchFloatError(a)
+    if type(a) is float:
+        b = input('Введите конечное значение: ')
+        b = CatchFloatError(b)
+        if type(b) is float:
+            if a >= b:
+                print('Некорректный ввод. ')
+            else:
+                printer(a, b, N1, N2)
+                eps = input('Введите эпсилон: ')
+                eps = CatchFloatError(eps)
+                if type(eps) is float:
+                    refinement(eps)
 
-def getAll():
-    getInterval()
-    getStep()
+'''if type(a) is float and type(b) is float\
+   and type(N1) is int and type(N2) is int and b > a:'''
     
-main()
 
-eps = float(input('Введите эпсилон: '))
-refinement(eps)
 
 input()
