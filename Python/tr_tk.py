97441546c9463ba076438af4c931ce33823e4f5d
The Light Betray Me <dr_gromova1998@mail.ru>
2017-12-11 11:21:08 +0300

    Update tr_tk.py

--- Python/tr_tk.py
+++ Python/tr_tk.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter.filedialog import *
+from ErrorCatcher import CatchIntError as cie
+from ErrorCatcher import CatchFloatError as cfe
 
 
 
@@ -14,30 +16,46 @@ class graph:
         self.canv_height = 200
         self.width = 600
         self.height = 700
+        #self.canv_width = 200
+        #self.canv_height = 200
         self.parent.resizable(width = False, height = False)#запрет на изменение размера
         self.canvas = tk.Canvas(root, height = self.height,\
                                 width = self.width, bg = 'light grey')
         self.canvas.grid(row = 0, column = 0)
-        self.canvas.create_rectangle(0, 10, 400, 600,
+        self.canvas.create_rectangle(0, 10, 400, 650,
                                      fill = 'white',
                                      outline = 'white')
         
         self.add_button = tk.Button(self.canvas, text = 'Add')
-        self.add_entry = tk.Entry(self.canvas, width = 15, bd = 3).place(x = 450, y = 50)
+        self.add_entry = tk.Entry(self.canvas, width = 15, bd = 3)
+        self.add_entry.place(x = 450, y = 50)
+        #self.add_entry.bind('<Return>', lambda e: self.getter)
         self.add_button.bind('<Button-1>', self.getter)
         self.add_button.place(x = 550, y = 50)
-        
         self.show_button = tk.Button(self.canvas, text = 'Show graph').place(x = 450, y = 100)
 
+        self.canvas.create_rectangle(450, 150, 590, 600,
+                                     fill = 'white',
+                                     outline = 'white')
+
+        self.clear_button = tk.Button(self.canvas, text = 'Clear all')
+        self.clear_button.bind('<Button-1>', self.clear_all)
+        self.clear_button.place(x = 500, y = 630)
         
+
+    
     
     def getter(self, event):
         new_coords = self.add_entry.get()
         print(new_coords)
+        self.add_entry.delete(0, END)
+    
 
     def shower(self):
         self.coords_text = tk.Label(self.canvas, text = 'dss\n23').place(x = 450, y = 150)
-        
+
+    def clear_all(self, event):
+        pass
 
 
 root = tk.Tk()
