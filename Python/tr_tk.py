1d009cf069fddcd877ed47cd3c58b18f53787399
The Light Betray Me <dr_gromova1998@mail.ru>
2017-12-11 11:44:57 +0300

    Update tr_tk.py

--- Python/tr_tk.py
+++ Python/tr_tk.py
@@ -16,9 +16,7 @@ class graph:
         self.canv_height = 200
         self.width = 600
         self.height = 700
-        #self.canv_width = 200
-        #self.canv_height = 200
-        self.parent.resizable(width = False, height = False)#запрет на изменение размера
+        self.parent.resizable(width = False, height = False) #запрет на изменение размера
         self.canvas = tk.Canvas(root, height = self.height,\
                                 width = self.width, bg = 'light grey')
         self.canvas.grid(row = 0, column = 0)
@@ -34,9 +32,9 @@ class graph:
         self.add_button.place(x = 550, y = 50)
         self.show_button = tk.Button(self.canvas, text = 'Show graph').place(x = 450, y = 100)
 
-        self.canvas.create_rectangle(450, 150, 590, 600,
-                                     fill = 'white',
-                                     outline = 'white')
+        #self.canvas.create_rectangle(450, 150, 590, 600,
+                                     #fill = 'white',
+                                     #outline = 'white')
 
         self.clear_button = tk.Button(self.canvas, text = 'Clear all')
         self.clear_button.bind('<Button-1>', self.clear_all)
@@ -47,13 +45,16 @@ class graph:
     
     def getter(self, event):
         new_coords = self.add_entry.get()
-        print(new_coords)
         self.add_entry.delete(0, END)
+        self.shower(new_coords)
+
+    def shower(self, new_coords):
+        text = tk.Text(root, width = 15, height = 25)
+        text.place(x = 450, y = 150)
+        text.config(takefocus=0)
+        text.insert(END, new_coords+'\n')
+        
     
-
-    def shower(self):
-        self.coords_text = tk.Label(self.canvas, text = 'dss\n23').place(x = 450, y = 150)
-
     def clear_all(self, event):
         pass
 
