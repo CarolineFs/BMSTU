3349d98a22a737da3c7d222f37c1519225f4d2e7
The Light Betray Me <dr_gromova1998@mail.ru>
2017-12-15 02:02:11 +0300

    Update and rename triangle affiliation.py to triangle_affiliation.py

--- Python/triangle_affiliation.py
+++ Python/triangle_affiliation.py
@@ -1,4 +1,10 @@
-def main(x, y):
+def main(x, y, set):
+    xa = set[0][0]
+    ya = set[0][1]
+    xb = set[1][0]
+    yb = set[1][1]
+    xc = set[2][0]
+    yc = set[2][1]
     f = 0
     if ((x - xa) * (ya - yb) - (y - ya) * (xa - xb) > 0):
         if ((x - xb) * (yb - yc) - (y - yb) * (xb - xc) > 0):
