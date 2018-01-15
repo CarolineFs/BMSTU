5a67c23db735be4af31414c858250917da787e4a
Lander <dr_gromova1998@mail.ru>
2018-01-15 03:08:12 +0300

    Update binary_insertion_sort.py

--- Sorting arrays/binary_insertion_sort.py
+++ Sorting arrays/binary_insertion_sort.py
@@ -1,20 +1,23 @@
-def binary_insertion(array):
-    for i in range(1, len(array)):
-        left = 0
-        right = i - 1
-        middle = (left + right+1)//2
-        print(middle)
-        while True:
-            print(array[i], array[middle])
-            if array[i] > array[middle]:
-                left = middle
-            else:
-                right = middle
-            middle = (left + right+1)//2
-            print(left, right, middle)
-            if middle == i - 1:
-                break
+array = [0, 4, 1, 6, 2, 6]
 
 
-array = [1,2,3,5,4]
-binary_insertion(array)
+def binary_insertion_sort(array):
+    for i in range(2, len(array)):
+        if array[i-1] > array[i]:
+            x = array[i]
+            left = 1
+            right = len(array) - 1
+            while True:
+                middle = (left + right)//2
+                if array[middle] < x:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+                if left > right:
+                    break
+            for j in range(left, i-1, -1):
+                array[j+1] = array[j]
+            array[left] = x
+    return array
+
+print(binary_insertion_sort(array))
