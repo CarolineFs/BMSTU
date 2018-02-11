989dfc978fd54292c8f22c103712179af57b8cdc
Lander <dr_gromova1998@mail.ru>
2018-02-11 13:16:15 +0300

    Update calc_worked.py

--- Python/2 semester/calc/calc_worked.py
+++ Python/2 semester/calc/calc_worked.py
@@ -1,40 +1,80 @@
 import tkinter as tk
+from ErrorCatcher import catch_float_error
+from quadric_equasion import solve_quadric_equasion
 
 # CONST
 CANVAS_WIDTH = 225
 CANVAS_HEIGHT = 480
 
 
-def getter(event, entry_a, entry_b, entry_c):
-    pass
+def show_result(x1, x2, entry_result):
+    print(x1, x2)
+    if x2 is not None:
+        res = 'x1='+str(x1)+', x2='+str(x2)
+        entry_result.delete(0, len(entry_result.get()))
+        entry_result.insert(0, res)
+    else:
+        if x1 is not None:
+            if x1 != 'Нет действительных корней':
+                res = 'x=' + str(x1)
+            else:
+                res = 'Нет действительных корней'
+        else:
+            res = 'Решений нет'
+        entry_result.delete(0, len(entry_result.get()))
+        entry_result.insert(0, res)
+
+
+def getter(event, entry_a, entry_b, entry_c, entry_result):
+    a = entry_a.get()
+    b = entry_b.get()
+    c = entry_c.get()
+    a = catch_float_error(a)
+    b = catch_float_error(b)
+    c = catch_float_error(c)
+    if (type(a) is float) and \
+        (type(b) is float) and \
+        (type(c) is float):
+        x1, x2 = solve_quadric_equasion(a, b, c)
+        show_result(x1, x2, entry_result)
+    else:
+        entry_result.delete(0, len(entry_result.get()))
+        entry_result.insert(0, 'Некорректный ввод')
+        if type(a) is not float:
+            entry_a.delete(0, len(entry_a.get()))
+        if type(b) is not float:
+            entry_b.delete(0, len(entry_b.get()))
+        if type(c) is not float:
+            entry_c.delete(0, len(entry_c.get()))
 
 
 def clear_all(event, entry_a, entry_b, entry_c, entry_result):
-    pass
+    entry_a.delete(0, len(entry_a.get()))
+    entry_b.delete(0, len(entry_b.get()))
+    entry_c.delete(0, len(entry_c.get()))
+    entry_result.delete(0, len(entry_result.get()))
 
 
 def clear_active_entry(event, root, entry_a, entry_b, entry_c, entry_result):
     entry = str(root.focus_displayof())
     if entry.endswith('entry'):
-        entry_a.delete(0)
+        entry_a.delete(0, len(entry_a.get()))
     elif entry.endswith('entry2'):
-        entry_b.delete(0)
+        entry_b.delete(0, len(entry_b.get()))
     elif entry.endswith('entry3'):
-        entry_c.delete(0)
-
-
-def get_result(event):
-    print('GET')
+        entry_c.delete(0, len(entry_c.get()))
+    elif entry.endswith('entry4'):
+        entry_result.delete(0, len(entry_result.get()))
 
 
 def insert(event, root, entry_a, entry_b, entry_c, string):
     entry = str(root.focus_displayof())
     if entry.endswith('entry'):
-        entry_a.insert(0, string)
+        entry_a.insert(len(entry_a.get()), string)
     elif entry.endswith('entry2'):
-        entry_b.insert(0, string)
+        entry_b.insert(len(entry_b.get()), string)
     elif entry.endswith('entry3'):
-        entry_c.insert(0, string)
+        entry_c.insert(len(entry_c.get()), string)
 
 
 def draw_canvas(root):
@@ -48,7 +88,7 @@ def draw_canvas(root):
                        bg='black')
     canvas.grid(row=0, column=0)
 
-    # Draw entries
+    # Create entries
     entry_a = tk.Entry(canvas, width=4, bg='SkyBlue1')
     entry_a.place(x=25, y=10)
 
@@ -69,7 +109,7 @@ def draw_canvas(root):
     entry_result = tk.Entry(canvas, width=27, bg='SkyBlue1')
     entry_result.place(x=25, y=35)
 
-    # Draw buttons
+    # Create buttons
     def handler_sign(event, root=root, entry_a=entry_a,
                 entry_b=entry_b, entry_c=entry_c, string='-'):
         return insert(event, root, entry_a, entry_b, entry_c, string)
@@ -92,7 +132,7 @@ def draw_canvas(root):
     button_ac.place(x=75, y=80)
 
     def handler_clear_active_entry(event, root=root, entry_a=entry_a,
-                          entry_b=entry_b, entry_c=entry_c, entry_result=entry_result):
+                                   entry_b=entry_b, entry_c=entry_c, entry_result=entry_result):
         return clear_active_entry(event, root, entry_a, entry_b, entry_c, entry_result)
     button_c = tk.Button(canvas, text='C', fg='white', bg='turquoise2',
                          font='Verdana 12', width=6, height=3,
@@ -212,12 +252,16 @@ def draw_canvas(root):
     button_point.bind('<Button-1>', handler_point)
     button_point.place(x=75, y=400)
 
+    def handler_getter(event, entry_a=entry_a, entry_b=entry_b,
+                       entry_c=entry_c, entry_result=entry_result):
+        return getter(event, entry_a, entry_b, entry_c, entry_result)
     button_equasion = tk.Button(canvas, text='=', fg='white', bg='turquoise2',
                                 font='Verdana 12', width=6, height=3,
                                 activebackground='turquoise3',
                                 activeforeground='white')
-    button_equasion.bind('<Button-1>', get_result)
+    button_equasion.bind('<Button-1>', handler_getter)
     button_equasion.place(x=150, y=400)
+
 
 
 def create_drop_menu(root):
@@ -231,13 +275,6 @@ def create_drop_menu(root):
     drop_menu.add_cascade(label='Clear', menu=first_item)
     first_item.add_command(label='Clear all')
     first_item.add_command(label='Clear result')
-
-
-def getter(entry_a, entry_b, entry_c):
-    a = entry_a.get()
-    b = entry_b.get()
-    c = entry_c.get()
-    return a, b, c
 
 
 def main():
