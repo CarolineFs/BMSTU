01e71044495e19d791dc5a6b0cf3cca8dd5ad08b
Lander <dr_gromova1998@mail.ru>
2018-03-15 13:36:26 +0300

    Update anagrams.py

--- Python/SE-labs/anagrams.py
+++ Python/SE-labs/anagrams.py
@@ -1,9 +1,7 @@
 '''
 Написать функцию для формирования строчки из случайных символов и записи в файл
-
 Написать функцию для чтения данных из файла и подсчета количества анаграмм
 в файле
-
 Функция для подсчета, сколько раз символ встретился в файле
 '''
 
@@ -26,6 +24,14 @@ def writer_random_str_in_file():
         f.write(random_str)
 
 
+def converter_word_into_array(word):
+    array = []
+    for i in word:
+        array.append(i)
+
+    return array
+
+
 def search_anagrams():
     with open('search_anagramms.txt', 'r', encoding='UTF-8') as f:
         text = f.read()
@@ -35,15 +41,40 @@ def search_anagrams():
 
     anagrams = 0
     for i in range(len(text)):
-        word1 = text[i]
+        word1 = converter_word_into_array(text[i])
         for j in range(i + 1, len(text)):
-            word2 = text[j]
-            for l in range(len(word2)):
-                if word2[l] in word1:
-                    word2.pop(l)
-                    word1.pop(word2[l])
+            word2 = converter_word_into_array(text[j])
+            if len(word2) == len(word1):
+                for i in text[i]:
+                    if i in word2:
+                        word2.remove(i)
+                        word1.remove(i)
+                        if len(word1) == len(word2) == 0:
+                            anagrams += 1
 
-        print(text)
+    print(anagrams)
 
 
-search_anagrams()
+def counter_symbol_in_file():
+    with open('count_symbol.txt', 'r', encoding='UTF-8') as f:
+        text = f.read()
+    symbol = input('\nВведите символ: ')
+    if len(symbol) != 1:
+        print('Это не один символ, но я посчитаю,' +\
+              'сколько раз эта последовательность символов встречается в файле. ')
+    k = text.count(symbol)
+    if k != 0:
+        print('Символ (последователность) "' + symbol + \
+              '" встречается в файле ' + \
+              str(k) + ' раз. ')
+    else:
+        print('Этот символ (последовательность) не встречается в файле. ')
+
+
+def main():
+    writer_random_str_in_file()
+    search_anagrams()
+    counter_symbol_in_file()
+
+
+main()
