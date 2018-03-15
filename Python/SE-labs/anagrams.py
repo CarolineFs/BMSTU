f203df17117cb0d51c977737a2e9558c0c668f50
Lander <dr_gromova1998@mail.ru>
2018-03-15 14:04:17 +0300

    Update anagrams.py

--- Python/SE-labs/anagrams.py
+++ Python/SE-labs/anagrams.py
@@ -7,20 +7,26 @@
 
 import random
 
+# CONST
+FILE_TO_WRITE_RANDOM_STR = 'random_str.txt'
+FILE_TO_SEARCH_ANAGRAMS = 'search_anagrams.txt'
+FILE_TO_COUNT_SYMBOL = 'count_symbol.txt'
+ENCODING = 'UTF - 8'
+
 
 def writer_random_str_in_file():
     symbols = 'qwertyuiopasdfghjklzxcvbnm' + \
               'йцукенгшщзхъэждлорпавфячсмитьбюё' +\
               '1234567890' +\
-              '!@#$%^&*()_+-=№;: '"><,./\[]{}|"
-    START = 0
-    END = len(symbols) - 1
+              '!@#$%^&*()_+-=№;: \'"><,./\[]{}|'
+    start = 0
+    end = len(symbols) - 1
     random_str = ''
     random_str_len = random.randrange(1, 50, 1)
     for i in range(random_str_len - 1):
-        random_str += symbols[random.randrange(START, END, 1)]
+        random_str += symbols[random.randrange(start, end, 1)]
 
-    with open('random_str.txt', 'w', encoding='UTF-8') as f:
+    with open(FILE_TO_WRITE_RANDOM_STR, 'w', encoding=ENCODING) as f:
         f.write(random_str)
 
 
@@ -28,12 +34,11 @@ def converter_word_into_array(word):
     array = []
     for i in word:
         array.append(i)
-
     return array
 
 
 def search_anagrams():
-    with open('search_anagramms.txt', 'r', encoding='UTF-8') as f:
+    with open(FILE_TO_SEARCH_ANAGRAMS, 'r', encoding=ENCODING) as f:
         text = f.read()
     text = text.split()
     for i in range(len(text)):
@@ -45,27 +50,28 @@ def search_anagrams():
         for j in range(i + 1, len(text)):
             word2 = converter_word_into_array(text[j])
             if len(word2) == len(word1):
-                for i in text[i]:
-                    if i in word2:
-                        word2.remove(i)
-                        word1.remove(i)
+                for k in text[i]:
+                    if k in word2:
+                        word2.remove(k)
+                        word1.remove(k)
                         if len(word1) == len(word2) == 0:
                             anagrams += 1
 
-    print(anagrams)
+    print('Количество анаграмм в файле ' + FILE_TO_SEARCH_ANAGRAMS +
+          ' : ' + str(anagrams))
 
 
 def counter_symbol_in_file():
-    with open('count_symbol.txt', 'r', encoding='UTF-8') as f:
+    with open(FILE_TO_COUNT_SYMBOL, 'r', encoding=ENCODING) as f:
         text = f.read()
     symbol = input('\nВведите символ: ')
     if len(symbol) != 1:
-        print('Это не один символ, но я посчитаю,' +\
+        print('Это не один символ, но я посчитаю,' +
               'сколько раз эта последовательность символов встречается в файле. ')
     k = text.count(symbol)
     if k != 0:
-        print('Символ (последователность) "' + symbol + \
-              '" встречается в файле ' + \
+        print('Символ (последователность) "' + symbol +
+              '" встречается в файле ' +
               str(k) + ' раз. ')
     else:
         print('Этот символ (последовательность) не встречается в файле. ')
