23ada9729af23a8ccd7eb9b7671fe4fc4fbe46bd
Lander <dr_gromova1998@mail.ru>
2018-03-19 19:34:06 +0300

    Update array_of_strings.py

--- C/array_of_strings.py
+++ C/array_of_strings.py
@@ -3,16 +3,21 @@
  * Функция, позволяющая редактировать одну из записей в массиве по номеру (вводятся номер и новая строка)
  * Функция, определяющая, сколько строк в массиве соответствуют введенной пользователем
 */
-
+/*
 #include <stdio.h>
 #include <string.h>
 #define ARRAY_LEN 10
-#define MAX_STR_LEN 255
+#define MAX_STR_LEN 20
 
 
-void* get_array(char* two_dim_array)
+char* get_array(char* two_dim_array)
 {
-    
+    for (unsigned int i = 0; i < ARRAY_LEN; ++i)
+    {
+        printf("Input string %u: ", i+1);
+        fgets(&two_dim_array[i], MAX_STR_LEN, stdin);
+    }
+    return *two_dim_array;
 }
 
 
@@ -20,21 +25,25 @@ int main()
 {
     char* two_dim_array[ARRAY_LEN][MAX_STR_LEN];
 
+    get_array(two_dim_array);
 
-   //get_array(two_dim_array);
+    /*for (unsigned int i = 0; i < ARRAY_LEN; ++i)
+    {
+        printf("Input string %u: ", i+1);
+        fgets(two_dim_array[i], MAX_STR_LEN, stdin);
+    }
 
-   for (unsigned int i = 0; i < ARRAY_LEN; ++i)
-   {
-       printf("Input string %u: ", i+1);
-       fgets(two_dim_array[i], MAX_STR_LEN, stdin);
-       fputs(two_dim_array[i], stdout);
-   }
+    for (unsigned int i = 0; i < ARRAY_LEN; ++i)
+    {
+        fputs(two_dim_array[i], stdout);
+    }
+
 
 
    return 0;
-}
+}*/
 
-/*
+
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,16 +51,17 @@ int main()
 
 int main(void)
 {
-    char* buf = NULL;
+    char* two_dim_array [10] = {NULL};
+    //char* buf = NULL;
     size_t len;
-
+    
     puts("Input a line:");
-    if(getline(&buf, &len, stdin) == -1 && ferror(stdin))
+    if(getline(&two_dim_array[0], &len, stdin) == -1 && ferror(stdin))
     err(1, "getline");
 
-    printf("You've entered %u character(s) (including newline):\n%s", strlen(buf), buf);
+    printf("You've entered %u character(s) (including newline):\n%s", strlen(two_dim_array[0]), two_dim_array[0]);
 
-    free(buf);
+    free(two_dim_array[0]);
 
     exit(0);
-}*/
+}
