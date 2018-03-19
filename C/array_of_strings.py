1eb062fef1c76d5ba660bbc425dcf3b8635645fd
Lander <dr_gromova1998@mail.ru>
2018-03-19 19:40:51 +0300

    Update array_of_strings.py

--- C/array_of_strings.py
+++ C/array_of_strings.py
@@ -1,67 +1,28 @@
-/*
- * Функция, позволяющая вносить новую строку с клавиатуры в массив (массив на 10 строк)
- * Функция, позволяющая редактировать одну из записей в массиве по номеру (вводятся номер и новая строка)
- * Функция, определяющая, сколько строк в массиве соответствуют введенной пользователем
-*/
-/*
-#include <stdio.h>
-#include <string.h>
-#define ARRAY_LEN 10
-#define MAX_STR_LEN 20
-
-
-char* get_array(char* two_dim_array)
-{
-    for (unsigned int i = 0; i < ARRAY_LEN; ++i)
-    {
-        printf("Input string %u: ", i+1);
-        fgets(&two_dim_array[i], MAX_STR_LEN, stdin);
-    }
-    return *two_dim_array;
-}
-
-
-int main()
-{
-    char* two_dim_array[ARRAY_LEN][MAX_STR_LEN];
-
-    get_array(two_dim_array);
-
-    /*for (unsigned int i = 0; i < ARRAY_LEN; ++i)
-    {
-        printf("Input string %u: ", i+1);
-        fgets(two_dim_array[i], MAX_STR_LEN, stdin);
-    }
-
-    for (unsigned int i = 0; i < ARRAY_LEN; ++i)
-    {
-        fputs(two_dim_array[i], stdout);
-    }
-
-
-
-   return 0;
-}*/
-
 
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define ARRAY_LEN 5
+
 int main(void)
 {
-    char* two_dim_array [10] = {NULL};
-    //char* buf = NULL;
+    char* two_dim_array [ARRAY_LEN] = {NULL};
     size_t len;
-    
-    puts("Input a line:");
-    if(getline(&two_dim_array[0], &len, stdin) == -1 && ferror(stdin))
-    err(1, "getline");
 
-    printf("You've entered %u character(s) (including newline):\n%s", strlen(two_dim_array[0]), two_dim_array[0]);
+    for (size_t i = 0; i < ARRAY_LEN; ++i)
+    {
+        puts("Input a line:");
+        if(getline(&two_dim_array[i], &len, stdin) == -1 && ferror(stdin))
+        err(1, "getline");
+        printf("You've entered %u character(s) (including newline):\n%s",
+               strlen(two_dim_array[i]), two_dim_array[i]);
+    }
 
-    free(two_dim_array[0]);
+
+    for (size_t i = 0; i < ARRAY_LEN; ++i)
+        free(two_dim_array[i]);
 
     exit(0);
 }
