e6c61d8243bca5f3bef430921b080bfd59448db5
Lander <dr_gromova1998@mail.ru>
2018-05-11 23:51:02 +0300

    Create geometry_managers.py

---
+++ Projects/PygubuFix/geometry_managers.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+
+class Layout:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, start_pos, size, parent):
+        self.mStartPos = start_pos
+        self.mSize = size
+        self.mParent = parent
+
+        self.frame = tk.Frame(self.mParent,
+                              height=self.mSize[1],
+                              width=self.mSize[0])
+
+        self.frame.place(x=self.mStartPos[0],
+                         y=self.mStartPos[1])
+
+    @abstractmethod
+    def set_item(self):
+        pass
+
+    @abstractmethod
+    def get_item(self):
+        pass
+
+    @abstractmethod
+    def remove_item(self):
+        pass
+
+    @abstractmethod
+    def move_item(self):
+        pass
+
+    @abstractmethod
+    def clear_all(self):
+        pass
+
+
+class GridLayout(Layout):
+    def __init__(self, start_pos, size, parent, col, row):
+        Layout.__init__(self, start_pos, size, parent)
+        self.mRow = row
+        self.mCol = col
+
+    def set_item(self):
+        pass
+
+    def get_item(self):
+        pass
+
+    def remove_item(self):
+        pass
+
+    def move_item(self):
+        pass
+
+    def resize_grid(self):
+        pass
+
+    def clear_all(self):
+        pass
+
+    def remove_grid(self):
+        pass
+
+
+class PlaceLayout(Layout):
+    def __init__(self, start_pos, size, parent):
+        Layout.__init__(self, start_pos, size, parent)
+
+    def set_item(self):
+        pass
+
+    def get_item(self):
+        pass
+
+    def remove_item(self):
+        pass
+
+    def move_item(self):
+        pass
+
+    def clear_all(self):
+        pass
+
+
