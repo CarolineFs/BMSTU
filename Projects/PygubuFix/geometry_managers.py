b75a38ef3583b0e88f440baf70291ab1488a1917
Lander <dr_gromova1998@mail.ru>
2018-05-12 13:47:09 +0300

    Update geometry_managers.py

--- Projects/PygubuFix/geometry_managers.py
+++ Projects/PygubuFix/geometry_managers.py
@@ -6,6 +6,7 @@ class Layout:
     __metaclass__ = ABCMeta
 
     def __init__(self, start_pos, size, parent):
+        ''' Фреймы размещаются только по коордиатам '''
         self.mStartPos = start_pos
         self.mSize = size
         self.mParent = parent
@@ -34,7 +35,7 @@ class Layout:
         pass
 
     @abstractmethod
-    def move_item(self, pos1, pos2, obj):
+    def move_item(self, new_x, new_y, obj):
         pass
 
     @abstractmethod
@@ -42,18 +43,26 @@ class Layout:
         pass
 
     @abstractmethod
-    def change_parent(self):
+    def change_layout_parent(self, new_parent):
+        pass
+
+    @abstractmethod
+    def move_layout(self, new_pos1, new_pos2):
         pass
 
 
 class GridLayout(Layout):
+    ''' Все методы возвращают None в случае ошибки '''
     def __init__(self, start_pos, size, parent, col, row):
         Layout.__init__(self, start_pos, size, parent)
         self.mRow = row
         self.mCol = col
 
     def set_item(self, row, col, obj):
-        obj.grid(row=row, column=col)
+        try:
+            obj.grid(row=row, column=col)
+        except AttributeError:
+            return None
 
     def get_item_by_position(self, row, col):
         ''' Возвращает объект на искомой позиции либо None '''
@@ -76,23 +85,35 @@ class GridLayout(Layout):
         return position
 
     def remove_item(self, obj):
-        obj.grid_forget()
-        return obj
+        try:
+            obj.grid_forget()
+            return obj
+        except AttributeError:
+            return None
 
-    def move_item(self, row, col, obj):
-        pass
-
-    def resize_grid(self):
-        pass
+    def move_item(self, new_row, new_col, obj):
+        try:
+            obj.grid_forget()
+            obj.grid(row=new_row, col=new_col)
+        except AttributeError:
+            return None
 
     def clear_all(self):
-        pass
+        forgot_items = []
+        try:
+            for item in self.frame.grid_slaves():
+                item.grid_forget()
+                forgot_items.append(item)
+        except AttributeError:
+            return None
+        return forgot_items
 
-    def remove_grid(self):
-        pass
+    def change_layout_parent(self, new_parent):
+        self.mParent = new_parent
 
-    def change_parent(self):
-        pass
+    def move_layout(self, new_x, new_y):
+        self.mStartPos[0] = new_x
+        self.mStartPos[1] = new_y
 
 
 class PlaceLayout(Layout):
@@ -100,55 +121,71 @@ class PlaceLayout(Layout):
         Layout.__init__(self, start_pos, size, parent)
 
     def set_item(self, x, y, obj):
-        obj.place(x=x, y=y)
+        try:
+            obj.place(x=x, y=y)
+        except AttributeError:
+            return None
 
     def get_item_by_position(self, x, y):
-        pass
+        ''' Возвращает объект на искомой позиции либо None '''
+        try:
+            for item in self.frame.place_slaves():
+                info = item.place_info()
+                if info['x'] == x and info['y'] == y:
+                    return item
+        finally:
+            return None
 
     def get_item_position(self, obj):
-        pass
+        ''' Возвращает позицию объекта в виде (ряд, столбец) либо None'''
+        try:
+            info = obj.place_info()
+        except AttributeError:
+            return None
+        else:
+            position = (info['x'], info['y'])
+            if len(info) == 0:
+                return None
+        return position
 
     def remove_item(self, obj):
-        pass
+        try:
+            obj.place_forget()
+            return obj
+        except AttributeError:
+            return None
 
-    def move_item(self, x, y, obj):
-        pass
+    def move_item(self, new_x, new_y, obj):
+        try:
+            obj.place_forget()
+            obj.grid(row=new_x, col=new_y)
+        except AttributeError:
+            return None
 
     def clear_all(self):
-        pass
+        forgot_items = []
+        try:
+            for item in self.frame.place_slaves():
+                item.place_forget()
+                forgot_items.append(item)
+        except AttributeError:
+            return None
+        return forgot_items
 
-    def change_parent(self):
-        pass
+    def change_layout_parent(self, new_parent):
+        self.mParent = new_parent
+
+    def move_layout(self, new_x, new_y):
+        self.mStartPos[0] = new_x
+        self.mStartPos[1] = new_y
 
 
 class VerticalLayout(GridLayout):
     def __init__(self, start_pos, size, row, parent):
         GridLayout.__init__(self, start_pos, size, parent, col=0, row=row)
-        self.mRow = row
 
-    def set_item(self, row, col, obj):
+    def set_item(self, row, obj, col=0):
         obj.grid(row=row, column=col)
-
-    def get_item_by_position(self, row, col=0):
-        pass
-
-    def get_item_position(self, obj):
-        info = obj.grid_info()
-        position = (info['row'], info['column'])
-        return position
-
-    def remove_item(self, obj):
-        obj.grid_forget()
-        return obj
-
-    def move_item(self, row, col, obj):
-        pass
-
-    def clear_all(self):
-        pass
-
-    def change_parent(self):
-        pass
 
 
 class HorizontalLayout(GridLayout):
@@ -158,42 +195,3 @@ class HorizontalLayout(GridLayout):
 
     def set_item(self, row, col, obj):
         obj.grid(row=row, column=col)
-
-    def get_item_by_position(self, col, row=0):
-        pass
-
-    def get_item_position(self, obj):
-        info = obj.grid_info()
-        position = (info['row'], info['column'])
-        return position
-
-    def remove_item(self, obj):
-        obj.grid_forget()
-        return obj
-
-    def move_item(self, row, col, obj):
-        pass
-
-    def clear_all(self):
-        pass
-
-    def change_parent(self):
-        pass
-
-
-root = tk.Tk()
-t = tk.Frame(root)
-t.grid(row=0, column=0)
-b = tk.Button(t, text='rrr')
-b.grid(row=0, column=0)
-
-b2 = tk.Button(t, text='zzz')
-b2.grid(row=1, column=1)
-
-b3 = tk.Button(root, text='xxx')
-b3.place(x=0,y=0)
-a = 9
-r = a.grid_info()
-print(r)
-
-
