f418080476a9d288c55f489072298a14fd06d696
Lander <dr_gromova1998@mail.ru>
2018-05-15 00:30:16 +0300

    Update TestVersion.py

--- Projects/PygubuFix/TestVersion.py
+++ Projects/PygubuFix/TestVersion.py
@@ -2,21 +2,22 @@ import tkinter as tk
 from abc import ABCMeta, abstractmethod
 
 
+
+
 class Layout:
     __metaclass__ = ABCMeta
 
-    def __init__(self, x, y, height, width, parent, background, bd, bg, borderwidth,
-                 colormap, container, cursor,  highlightbackground, highlightcolor,
-                 highlightthickness, relief, takefocus, visual,
-                 relx, rely, relwidth, relheight, anchor, bordermode):
+    def __init__(self, x, y, height, width, parent, bd=0,
+                 bg='SystemButtonFace', colormap='', container=0, cursor='',
+                 highlightbackground='SystemButtonFace', highlightcolor='SystemButtonFace',
+                 highlightthickness=0, relief='flat', takefocus=0, visual='', relx='0', rely=0,
+                 relwidth='', relheight='', anchor='nw', bordermode='inside'):
         """ Фреймы размещаются только по координатам """
         self.mX = x
         self.mY = y
         self.mParent = parent
-        self.mBackground = background
         self.mBd = bd
         self.mBg = bg
-        self.mBorderwidth = borderwidth
         self.mColormap = colormap
         self.mContainer = container
         self.mCursor = cursor
@@ -34,8 +35,7 @@ class Layout:
         self.mRelheight = relheight
         self.mAnchor = anchor
         self.mBordermode = bordermode
-        self.frame = tk.Frame(self.mParent, background=self.mBackground, bd=self.mBd, bg=self.mBg,
-                              borderwidth=self.mBorderwidth, colormap=self.mColormap,
+        self.frame = tk.Frame(self.mParent, bd=self.mBd, bg=self.mBg, colormap=self.mColormap,
                               container=self.mContainer, cursor=self.mCursor, height=self.mHeight,
                               highlightbackground=self.mHighlightbackground, highlightcolor=self.mHighlightcolor,
                               highlightthickness=self.mHighlightthickness, relief=self.mRelief,
@@ -43,17 +43,10 @@ class Layout:
         self.frame.place(x=self.mX, y=self.mY, relx=self.mRelx, rely=self.mRely, width=self.mWidth,
                          height=self.mHeight, relwidth=self.mRelwidth, relheight=self.mRelheight,
                          anchor=self.mAnchor, bordermode=self.mBordermode)
+        self.frame.grid_propagate(False)
 
     @abstractmethod
     def set_item(self, pos1, pos2, obj):
-        pass
-
-    @abstractmethod
-    def get_item_by_position(self, pos1, pos2):
-        pass
-
-    @abstractmethod
-    def get_item_properties(self, obj):
         pass
 
     @abstractmethod
@@ -61,19 +54,12 @@ class Layout:
         pass
 
     @abstractmethod
-    def move_item(self, new_x, new_y, obj):
-        pass
-
-    @abstractmethod
     def clear_all(self):
         pass
 
-    @abstractmethod
     def update_layout_properties(self):
-        self.frame['background'] = self.mBackground
         self.frame['bd'] = self.mBd
         self.frame['bg'] = self.mBg
-        self.frame['borderwidth'] = self.mBorderwidth
         self.frame['colormap'] = self.mColormap
         self.frame['container'] = self.mContainer
         self.frame['cursor'] = self.mCursor
@@ -86,14 +72,10 @@ class Layout:
         self.frame['visual'] = self.mVisual
         self.frame['width'] = self.mWidth
 
-    @abstractmethod
     def get_layout_properties(self):
         info = dict()
-        info['background'] = self.mBackground
         info['bd'] = self.mBd
         info['bg'] = self.mBg
-        info['borderwidth'] = self.mBorderwidth
-        info['borderwidth'] = self.mBorderwidth
         info['colormap'] = self.mColormap
         info['container'] = self.mContainer
         info['cursor'] = self.mCursor
@@ -107,18 +89,30 @@ class Layout:
         info['width'] = self.mWidth
         return info
 
-    @abstractmethod
     def get_layout_place_properties(self):
         return self.frame.place_info()
+
+    def destroy_layout(self):
+        self.frame.destroy()
 
 
 class GridLayout(Layout):
     """ Все методы возвращают None в случае ошибки """
-    def __init__(self, start_pos, size, parent):
-        Layout.__init__(self, start_pos, size, parent)
+    def __init__(self, x, y, height, width, parent, bd=0, bg='SystemButtonFace', colormap='',
+                 container=0, cursor='', highlightbackground='SystemButtonFace',
+                 highlightcolor='SystemButtonFace', highlightthickness=0, relief='flat',
+                 takefocus=0, visual='', relx='0', rely=0, relwidth='', relheight='',
+                 anchor='nw', bordermode='inside'):
+        Layout.__init__(self, x=x, y=y, height=height, width=width, parent=parent,
+                        bd=bd, bg=bg, colormap=colormap, container=container, cursor=cursor,
+                        highlightbackground=highlightbackground, highlightcolor=highlightcolor,
+                        highlightthickness=highlightthickness, relief=relief, takefocus=takefocus,
+                        visual=visual, relx=relx, rely=rely, relwidth=relwidth,
+                        relheight=relheight, anchor=anchor, bordermode=bordermode)
 
     def set_item(self, row, col, obj, columnspan=1, rowspan=1, ipadx=0, ipady=0,
                  padx=0, pady=0, sticky=''):
+
         try:
             obj.grid(row=row, column=col, columnspan=columnspan, rowspan=rowspan,
                      ipadx=ipadx, ipady=ipady, padx=padx, pady=pady, sticky=sticky)
@@ -126,15 +120,16 @@ class GridLayout(Layout):
             return None
 
     def get_item_by_position(self, row, col):
-        ''' Возвращает объект на искомой позиции либо None '''
+        """ Возвращает объект на искомой позиции либо None """
         for item in self.frame.grid_slaves():
             info = item.grid_info()
             if info['row'] == row and info['column'] == col:
                 return item
         return None
 
-    def get_item_properties(self, obj):
-        ''' Возвращает позицию объекта в виде (ряд, столбец) либо None'''
+    @staticmethod
+    def get_item_grid_properties(obj):
+        """ Возвращает словарь grid-свойств объекта либо None"""
         try:
             info = obj.grid_info()
         except AttributeError:
@@ -144,19 +139,16 @@ class GridLayout(Layout):
                 return None
         return info
 
+    @staticmethod
+    def get_item_properties(obj):
+        """ Возвращает словарь свойств либо None """
+        info = dict(obj)
+        return info
+
     def remove_item(self, obj):
         try:
             obj.grid_forget()
             return obj
-        except AttributeError:
-            return None
-
-    def move_item(self, new_row, new_col, obj, columnspan=1, rowspan=1, ipadx=0, ipady=0,
-                  padx=0, pady=0, sticky=''):
-        try:
-            obj.grid_forget()
-            obj.grid(row=new_row, column=new_col, columnspan=columnspan, rowspan=rowspan,
-                     ipadx=ipadx, ipady=ipady, padx=padx, pady=pady, sticky=sticky)
         except AttributeError:
             return None
 
@@ -170,43 +162,49 @@ class GridLayout(Layout):
             return None
         return forgot_items
 
-    def change_layout_parent(self, new_parent):
-        self.mParent=new_parent
-
-    def move_layout(self, new_pos1, new_pos2):
-        super().move_layout(new_pos1, new_pos2)
-
-    def get_layout_properties(self):
-        return super().get_layout_properties()
-
     def get_slaves(self):
         return self.frame.grid_slaves()
 
 
 class PlaceLayout(Layout):
-    def __init__(self, start_pos, size, parent):
-        Layout.__init__(self, start_pos, size, parent)
+    def __init__(self, x, y, height, width, parent, bd=0, bg='SystemButtonFace', colormap='',
+                 container=0, cursor='', highlightbackground='SystemButtonFace',
+                 highlightcolor='SystemButtonFace', highlightthickness=0, relief='flat',
+                 takefocus=0, visual='', relx='0', rely=0, relwidth='', relheight='',
+                 anchor='nw', bordermode='inside'):
+        Layout.__init__(self, x=x, y=y, height=height, width=width, parent=parent,
+                        bd=bd, bg=bg, colormap=colormap, container=container, cursor=cursor,
+                        highlightbackground=highlightbackground, highlightcolor=highlightcolor,
+                        highlightthickness=highlightthickness, relief=relief, takefocus=takefocus,
+                        visual=visual, relx=relx, rely=rely, relwidth=relwidth,
+                        relheight=relheight, anchor=anchor, bordermode=bordermode)
 
     def set_item(self, obj, x=0, y=0,  relx=0, rely=0,
                  width='', height='', relheight='', relwidth='',
                  anchor='nw', bordermode='inside'):
         try:
-            obj.place(x=x, y=y)
+            obj.place(x=x, y=y, relx=relx, rely=rely, width=width,
+                      height=height, relheight=relheight, relwidth=relwidth,
+                      anchor=anchor, bordermode=bordermode)
         except AttributeError:
             return None
 
-    def get_item_by_position(self, x, y):
-        ''' Возвращает объект на искомой позиции либо None '''
+    def get_item_by_position(self, x, y, anchor, relx=0, rely=0):
+        """ Возвращает объект на искомой позиции либо None """
         try:
             for item in self.frame.place_slaves():
                 info = item.place_info()
-                if info['x'] == x and info['y'] == y:
+                if info['x'] == x and \
+                   info['y'] == y and\
+                   info['anchor'] == anchor and\
+                   info['relx'] == relx and\
+                   info['rely'] == rely:
                     return item
         finally:
             return None
 
-    def get_item_properties(self, obj):
-        ''' Возвращает позицию объекта в виде (ряд, столбец) либо None'''
+    @staticmethod
+    def get_item_properties(obj):
         try:
             info = obj.place_info()
         except AttributeError:
@@ -223,15 +221,6 @@ class PlaceLayout(Layout):
         except AttributeError:
             return None
 
-    def move_item(self, obj, new_x=0, new_y=0,  new_relx=0, new_rely=0,
-                  new_width='', new_height='', new_relheight='', new_relwidth='',
-                  new_anchor='nw', new_bordermode='inside'):
-        try:
-            obj.place_forget()
-            obj.place(x=new_x, y=new_y)
-        except AttributeError:
-            return None
-
     def clear_all(self):
         forgot_items = []
         try:
@@ -242,33 +231,53 @@ class PlaceLayout(Layout):
             return None
         return forgot_items
 
+    def get_slaves(self):
+        return self.frame.place_slaves()
 
+    @staticmethod
+    def get_item_place_properties(obj):
+        """ Возвращает словарь grid-свойств объекта либо None"""
+        try:
+            info = obj.place_info()
+        except AttributeError:
+            return None
+        else:
+            if len(info) == 0:
+                return None
+        return info
 
 
 class VerticalLayout(GridLayout):
-    def __init__(self, start_pos, size, row, parent):
-        GridLayout.__init__(self, start_pos, size, parent)
+    def __init__(self, x, y, height, width, parent, bd=0, bg='SystemButtonFace', colormap='',
+                 container=0, cursor='', highlightbackground='SystemButtonFace',
+                 highlightcolor='SystemButtonFace', highlightthickness=0, relief='flat',
+                 takefocus=0, visual='', relx='0', rely=0, relwidth='', relheight='',
+                 anchor='nw', bordermode='inside'):
+        GridLayout.__init__(self, x, y, height, width, parent, bd=bd, bg=bg, colormap=colormap,
+                            container=container, cursor=cursor, highlightbackground=highlightbackground,
+                            highlightcolor=highlightcolor, highlightthickness=highlightthickness,
+                            relief=relief, takefocus=takefocus, visual=visual, relx=relx, rely=rely,
+                            relwidth=relwidth, relheight=relheight, anchor=anchor, bordermode=bordermode)
 
-    def setItem(self, row, col, obj, columnspan=1, rowspan=1, ipadx=0, ipady=0,
-                 padx=0, pady=0, sticky=''):
-        obj.grid(row=row, column=col, columnspan=columnspan, rowspan=rowspan,
-                     ipadx=ipadx, ipady=ipady, padx=padx, pady=pady, sticky=sticky)
+    def set_item(self, row, obj, columnspan=1, rowspan=1, ipadx=0, ipady=0,
+                 padx=0, pady=0, sticky='', col=0):
+        obj.grid(row=row, column=0, columnspan=columnspan, rowspan=rowspan,
+                 ipadx=ipadx, ipady=ipady, padx=padx, pady=pady, sticky=sticky)
 
 
 class HorizontalLayout(GridLayout):
-    def __init__(self, start_pos, size, col, parent):
-        GridLayout.__init__(self, start_pos, size, parent)
-        self.mCol = col
+    def __init__(self, x, y, height, width, parent, bd=0, bg='SystemButtonFace', colormap='',
+                 container=0, cursor='', highlightbackground='SystemButtonFace',
+                 highlightcolor='SystemButtonFace', highlightthickness=0, relief='flat',
+                 takefocus=0, visual='', relx='0', rely=0, relwidth='', relheight='',
+                 anchor='nw', bordermode='inside'):
+        GridLayout.__init__(self, x, y, height, width, parent, bd=bd, bg=bg, colormap=colormap,
+                            container=container, cursor=cursor, highlightbackground=highlightbackground,
+                            highlightcolor=highlightcolor, highlightthickness=highlightthickness,
+                            relief=relief, takefocus=takefocus, visual=visual, relx=relx, rely=rely,
+                            relwidth=relwidth, relheight=relheight, anchor=anchor, bordermode=bordermode)
 
-    def setItem(self, row, col, obj, columnspan=1, rowspan=1, ipadx=0, ipady=0,
-                 padx=0, pady=0, sticky=''):
-        obj.grid(row=row, column=col, columnspan=columnspan, rowspan=rowspan,
-                     ipadx=ipadx, ipady=ipady, padx=padx, pady=pady, sticky=sticky)
-
-
-root = tk.Tk()
-l1 = tk.Frame(root, bg='red', width=100, height=100)
-l1.place(x=100, y=100)
-print(l1.place_info())
-
-root.mainloop()
+    def set_item(self, col, obj, columnspan=1, rowspan=1, ipadx=0, ipady=0,
+                 padx=0, pady=0, sticky='', row=0):
+        obj.grid(row=0, column=col, columnspan=columnspan, rowspan=rowspan,
+                 ipadx=ipadx, ipady=ipady, padx=padx, pady=pady, sticky=sticky)
