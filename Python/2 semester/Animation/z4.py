b793941f6b3d3e6f76c9ee5e184f90d34b49c3c3
Lander <dr_gromova1998@mail.ru>
2018-06-04 14:37:34 +0300

    Update z4.py

--- Python/2 semester/Animation/z4.py
+++ Python/2 semester/Animation/z4.py
@@ -1,43 +1,67 @@
-import pygame
-from math import sin, cos
+import sys
+from pygame import *
+from math import sin, cos, pi
 
-SIZE = (900, 700)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-WINDOW_NAME = 'Defend4'
+clock = time.Clock()
+size = width, height = 500, 500
+screen = display.set_mode(size)
+display.set_caption('Catch Birds')
 
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+BLUE = 14, 33, 137
+GREEN = 44, 144, 44
+RED = 144, 44, 44
+YELLOW = 204, 204, 44
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode(SIZE)
-    pygame.display.set_caption(WINDOW_NAME)
-    done = False
-    clock = pygame.time.Clock()
-    c_center = [70, 400]
-    eye1 = [[c_center[0]-30, c_center[1]-30], [c_center[0]-30, c_center[1]-20],
-            [c_center[0]-20, c_center[1]-20], [c_center[0]-20, c_center[1]-30]]
-    eye2 = [[c_center[0]+30, c_center[1]-30], [c_center[0]+30, c_center[1]-20],
-            [c_center[0]+20, c_center[1]-20], [c_center[0]+20, c_center[1]-30]]
-    mouth = [[c_center[0]-10, c_center[1]+10], [c_center[0]+10,c_center[1]+10],
-             [c_center[0], c_center[1]+20]]
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            screen.fill(WHITE)
-
-            pygame.draw.circle(screen, BLACK, c_center, 70, 3)
-            #pygame.draw.polygon(screen, BLACK, eye1, 3)
-            #pygame.draw.polygon(screen, BLACK, eye2, 3)
-            #pygame.draw.polygon(screen, BLACK, mouth, 3)
-
-            c_center[0] += 1
-
-            clock.tick(1000)
-            pygame.display.flip()
-
-    pygame.quit()
+angle = 0
+angle_t = 0
 
 
-main()
+def triangle(angle, angle_t):
+    r = 100
+    r_t = 50
+    c_x, c_y = 250, 250
+
+    center_x = 250 + r * cos(angle)
+    center_y = 250 + r * sin(angle)
+
+    top_x = center_x + r_t * cos(angle_t)
+    top_y = center_y + r_t * sin(angle_t)
+    down_l_x = center_x + r_t * cos(angle_t + (2 * pi / 3))
+    down_l_y = center_y + r_t * sin(angle_t + (2 * pi / 3))
+    down_r_x = center_x + r_t * cos(angle_t + (4 * pi / 3))
+    down_r_y = center_y + r_t * sin(angle_t + (4 * pi / 3))
+
+    left_x = (top_x + down_l_x) / 2
+    left_y = (top_y + down_l_y) / 2
+    right_x = (top_x + down_r_x) / 2
+    right_y = (top_y + down_r_y) / 2
+    down_x = (down_l_x + down_r_x) / 2
+    down_y = (down_l_y + down_r_y) / 2
+
+    draw.polygon(screen, BLUE, ((top_x, top_y), (left_x, left_y),
+                                (right_x, right_y)), 0)
+    draw.polygon(screen, GREEN, ((down_x, down_y), (left_x, left_y),
+                                 (right_x, right_y)), 0)
+    draw.polygon(screen, RED, ((down_x, down_y), (down_r_x, down_r_y),
+                               (right_x, right_y)), 0)
+    draw.polygon(screen, YELLOW, ((down_x, down_y), (left_x, left_y),
+                                  (down_l_x, down_l_y)), 0)
+
+
+while 1:
+    clock.tick(5)
+    for e in event.get():
+        if e.type == QUIT:
+            quit()
+            sys.exit()
+
+    angle += pi / 32
+    angle_t += pi / 8
+
+    screen.fill(WHITE)
+    draw.circle(screen, BLACK, (250, 250), 100, 1)
+    triangle(angle, angle_t)
+
+    display.flip()
